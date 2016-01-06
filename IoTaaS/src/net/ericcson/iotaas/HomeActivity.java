1609ba59db439a7812256e06c3a4af8eaa97ea88
Ozan <ozankonur@gmail.com>
2016-01-06 16:55:49 +0200

    First update

--- IoTaaS/src/net/ericcson/iotaas/HomeActivity.java
+++ IoTaaS/src/net/ericcson/iotaas/HomeActivity.java
@@ -20,9 +20,17 @@ public class HomeActivity extends Activity {
 		setContentView(R.layout.activity_home);
 		try {
 			String url="http://129.192.210.50:38080/rest/utilityData/meterReadings";
-
 			URL object=new URL(url);
+			AttempReadData(object,"16714748");
+		} catch (Exception e) {
+		}
 
+	}
+
+	public boolean AttempReadData(URL object,String objname)
+	{
+
+		try {
 			HttpURLConnection con = (HttpURLConnection) object.openConnection();
 
 			con.setDoOutput(true);
@@ -43,17 +51,18 @@ public class HomeActivity extends Activity {
 			OutputStream out = con.getOutputStream();
 			OutputStreamWriter wout = new OutputStreamWriter(out, "UTF-8");
 
-			wout.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?><tns:CreateMeterReadings xmlns:tns=\"http://iec.ch/TC57/2011/MeterReadingsMessage\" xmlns:msg=\"http://iec.ch/TC57/2011/schema/message\" xmlns:obj=\"http://iec.ch/TC57/2011/MeterReadings#\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://iec.ch/TC57/2011/MeterReadingsMessage ../xsd/MeterReadingsMessage.xsd\"><tns:Header><msg:Verb>create</msg:Verb><msg:Noun>MeterReadings</msg:Noun><msg:Revision>0.1</msg:Revision><msg:Timestamp>2014-12-17T09:30:41.0Z</msg:Timestamp><msg:ReplyAddress>http://129.192.210.36:8080/Thingworx/Things/IntegrationMasterThing/Services/handleDataPull?postParameter=body</msg:ReplyAddress><msg:CorrelationID>123123</msg:CorrelationID></tns:Header><tns:Payload><obj:MeterReadings><obj:MeterReading><obj:Meter><obj:Names><obj:name>16714748</obj:name></obj:Names></obj:Meter></obj:MeterReading><obj:ReadingType><obj:Names><obj:name>0.0.0.1.1.1.12.0.0.0.0.0.0.0.0.3.72.0</obj:name></obj:Names></obj:ReadingType></obj:MeterReadings></tns:Payload></tns:CreateMeterReadings>");
+			wout.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?><tns:CreateMeterReadings xmlns:tns=\"http://iec.ch/TC57/2011/MeterReadingsMessage\" xmlns:msg=\"http://iec.ch/TC57/2011/schema/message\" xmlns:obj=\"http://iec.ch/TC57/2011/MeterReadings#\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://iec.ch/TC57/2011/MeterReadingsMessage ../xsd/MeterReadingsMessage.xsd\"><tns:Header><msg:Verb>create</msg:Verb><msg:Noun>MeterReadings</msg:Noun><msg:Revision>0.1</msg:Revision><msg:Timestamp>2014-12-17T09:30:41.0Z</msg:Timestamp><msg:ReplyAddress>http://129.192.210.36:8080/Thingworx/Things/IntegrationMasterThing/Services/handleDataPull?postParameter=body</msg:ReplyAddress><msg:CorrelationID>123123</msg:CorrelationID></tns:Header><tns:Payload><obj:MeterReadings><obj:MeterReading><obj:Meter><obj:Names><obj:name>"+objname+"</obj:name></obj:Names></obj:Meter></obj:MeterReading><obj:ReadingType><obj:Names><obj:name>0.0.0.1.1.1.12.0.0.0.0.0.0.0.0.3.72.0</obj:name></obj:Names></obj:ReadingType></obj:MeterReadings></tns:Payload></tns:CreateMeterReadings>");
 
 			wout.flush();
 			out.close();
 
 			String result=readInputStreamToString(con);
 			System.out.println(result);
-			
-		} catch (Exception e) {
-		}
+			return true;
 
+		} catch (Exception e) {
+			return false;
+		}
 	}
 
 	static public String readInputStreamToString(HttpURLConnection connection) {
